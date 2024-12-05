dbd8078d989b4eaff3731110e43d2b7a27835635
Dmitry Konstantinov <dm3ypost@gmail.com>
2024-12-05 22:59:49 +0300

    Add app.get(/courses/{id}) and app.get(/courses)

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,14 +1,25 @@
 package org.example.hexlet;
 
 import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
+
+import java.util.List;
+
+import static io.javalin.rendering.template.TemplateUtil.model;
+
+import org.example.hexlet.model.Course;
+import org.example.hexlet.dto.courses.CoursePage;
 
 public class HelloWorld {
     public static void main(String[] args) {
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
         });
+
         app.get("/users", ctx -> ctx.result("GET /users"));
         app.post("/users", ctx -> ctx.result("POST /users"));
+
         app.get("/hello", ctx -> {
             String name = ctx.queryParam("name");
             if (name == null || name.isEmpty()) {
@@ -17,11 +28,44 @@ public class HelloWorld {
                 ctx.result("Hello, " + name + "!");
             }
         });
+
         app.get("/users/{id}/post/{postId}", ctx -> {
             var usersId = ctx.pathParam("id");
-            var postId =  ctx.pathParam("postId");
+            var postId = ctx.pathParam("postId");
             ctx.result("Users ID: " + usersId + " Post ID: " + postId);
         });
+
+        app.get("/courses/{id}", ctx -> {
+            var id = Long.parseLong(ctx.pathParam("id"));
+            List<Course> courses = List.of(
+                    new Course(1L, "Java", "Основы Java, программирование на Java."),
+                    new Course(2L, "Python", "Введение в Python, анализ данных с помощью Python."),
+                    new Course(3L, "JavaScript", "Изучение основ JavaScript для веб-разработки."),
+                    new Course(4L, "Ruby", "Введение в Ruby и создание веб-приложений с Ruby on Rails."));
+            Course course = courses.stream()
+                    .filter(c -> c.getId().equals(id))
+                    .findFirst()
+                    .orElse(null);
+            var header = "Курсы по программированию";
+            if (course == null) {
+                ctx.status(404).result("Курс не найден");
+            } else {
+                var page = new CoursePage(List.of(course), header);
+                ctx.render("courses/show.jte", model("page", page));
+            }
+        });
+        app.get("/courses", ctx -> {
+            List<Course> courses = List.of(
+                    new Course(1L, "Java", "Основы Java, программирование на Java."),
+                    new Course(2L, "Python", "Введение в Python, анализ данных с помощью Python."),
+                    new Course(3L, "JavaScript", "Изучение основ JavaScript для веб-разработки."),
+                    new Course(4L, "Ruby", "Введение в Ruby и создание веб-приложений с Ruby on Rails."));
+
+            var header = "Курсы по программированию";
+            var page = new CoursePage(courses, header);
+            ctx.render("courses/index.jte", model("page", page));
+        });
+
         app.start(7070);
     }
 }
