d127019e2b4f13e4ee4b4a86f42d1e7e4bdba75e
Dmitry Konstantinov <dm3ypost@gmail.com>
2024-12-13 22:27:38 +0300

    Add NamedRoutes.java

---
+++ src/main/java/org/example/hexlet/NamedRoutes.java
@@ -0,0 +1,42 @@
+package org.example.hexlet;
+
+public class NamedRoutes {
+
+    // Маршрут пользователей
+    public static String usersPath() {
+        return "/users";
+    }
+
+    // Маршрут формы добавления пользователей
+    public static String buildUserPath() {
+        return "/users/build";
+    }
+
+    // Это нужно, чтобы не преобразовывать типы снаружи
+    public static String usersPath(Long id) {
+        return usersPath(String.valueOf(id));
+    }
+
+    public static String usersPath(String id) {
+        return "/users/" + id;
+    }
+
+    // Маршрут формы добавления курсов
+    public static String buildCoursesPath() {
+        return "/courses/build";
+    }
+
+    // Маршрут курсов
+    public static String coursesPath() {
+        return "/courses";
+    }
+
+    // Это нужно, чтобы не преобразовывать типы снаружи
+    public static String coursePath(Long id) {
+        return coursePath(String.valueOf(id));
+    }
+
+    public static String coursePath(String id) {
+        return "/courses/" + id;
+    }
+}
