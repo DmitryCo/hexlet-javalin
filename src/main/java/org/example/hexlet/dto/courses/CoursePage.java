fdc5930f18b6df2d4b9e33537869fbe9f8f0fc38
Dmitry Konstantinov <dm3ypost@gmail.com>
2024-12-19 01:03:08 +0300

    Add extends BasePage

--- src/main/java/org/example/hexlet/dto/courses/CoursePage.java
+++ src/main/java/org/example/hexlet/dto/courses/CoursePage.java
@@ -1,15 +1,17 @@
 package org.example.hexlet.dto.courses;
 
+
 import java.util.List;
 
 import org.example.hexlet.model.Course;
+import org.example.hexlet.dto.main.BasePage;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
-public class CoursePage {
+public class CoursePage extends BasePage {
     private List<Course> courses;
-    public String term;
+    private String term;
 }
